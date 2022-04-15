51234cd6e1e4d3082f405eb734e1f4231e50f0b4
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-04-15 11:41:56 +0200

    Removing speedtest django app

--- scripts/pingtest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source YOUR_VIRTUAL_ENV
-cd YOUR_PROJECT_DIRECTORY || exit
-
-python manage.py run_speedtest --short
-
-finished=$(date '+%d/%m/%Y %H:%M:%S')
-echo "Last pingtest run done at ${finished}"
