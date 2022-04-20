1af1cc96287e66bf2d8e64e62f73dfb4b634c448
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-04-20 12:39:27 +0200

    Prettier dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from dashboard.models import MenuURL
+from dashboard.models import MenuURL, UrlGroup
 
 
 @admin.register(MenuURL)
 class MenuURLAdmin(admin.ModelAdmin):
-    list_display = ('text', 'url', 'active', 'order', 'created', 'updated')
+    list_display = ('text', 'url', 'new_window', 'active', 'order', 'url_group', 'created', 'updated')
+
+
+@admin.register(UrlGroup)
+class UrlGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text', 'active')
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from dashboard.models import MenuURL
+from dashboard.models import MenuURL, UrlGroup
 
 
 def index(request):
     context = {
-        'menu_items': MenuURL.objects.filter(active=True)
+        'url_groups': UrlGroup.objects.filter(active=True),
+        'menu_items': MenuURL.objects.filter(active=True, url_group=None)
     }
     template = 'dashboard/index.html'
     return render(request, template, context)
