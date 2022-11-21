c05eb21c5cf469c4064c548e97d0084c2469178b
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-11-21 13:34:48 +0100

    Removal of monitor app part 1 - remove models

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
 
-from monitor.models import Device, Scan
-
-
-@admin.register(Device)
-class DeviceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'first_seen', 'last_seen', 'mac_address', 'last_ip', 'scan_count')
-    search_fields = ('name', 'type', 'manufacturer', 'last_ip', 'mac_address')
-
-    def scan_count(self, obj):
-        return obj.scan_set.count()
-
-
-@admin.register(Scan)
-class ScanAdmin(admin.ModelAdmin):
-    list_display = ('time', 'type', 'command')
--- monitor/urls.py
+++ monitor/urls.py
@@ -1,17 +1,5 @@
-from django.urls import path
-
-from monitor import views
 
 app_name = 'monitor'
 
 urlpatterns = [
-    path('', views.device_day, name='index'),
-    path('device/all/', views.device_all, name='device_all'),
-    path('device/<date:day>/', views.device_day, name='device_day'),
-    path('device/filter/', views.device_filter, name='device_filter'),
-    path('device/<int:device_id>/', views.device_details, name='device_details'),
-    path('device/edit/<int:device_id>/', views.device_edit, name='device_edit'),
-    path('device/history/<int:device_id>/', views.device_history, name='device_history'),
-    path('scan/start/', views.scan_start, name='scan_start'),
-    path('scan/<int:device_id>/', views.scan_details, name='scan_details'),
 ]
