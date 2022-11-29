6816394e494623e562055ddf44cd514c8d37a2fd
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-11-29 10:24:13 +0100

    Including nginx and gunicorn templates for setting up

--- scripts/inventory.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source YOUR_VIRTUAL_ENV
-cd YOUR_PROJECT_DIRECTORY || exit
-
-python manage.py scan
-
-finished=$(date '+%d/%m/%Y %H:%M:%S')
-echo "Last inventory run done at ${finished}"
