b6cf9bda68dd96617f4ab4ee9ae26203040cc9c4
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-12-03 19:24:44 +0100

    Filters in admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,9 +5,11 @@ from dashboard.models import MenuURL, UrlGroup
 
 @admin.register(MenuURL)
 class MenuURLAdmin(admin.ModelAdmin):
-    list_display = ('text', 'url', 'new_window', 'active', 'order', 'url_group', 'created', 'updated')
+    list_display = ('text', 'url', 'new_window', 'active', 'post_request', 'order', 'url_group', 'created', 'updated')
+    list_filter = ('url_group', 'active', 'new_window', 'post_request')
 
 
 @admin.register(UrlGroup)
 class UrlGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'text', 'active')
+    list_filter = ('active',)
