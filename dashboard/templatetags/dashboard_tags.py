bd5d7ac3728871dd685a03f051e95a657fe32135
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-12-03 19:16:41 +0100

    Fixes. wip.

--- dashboard/templatetags/dashboard_tags.py
+++ dashboard/templatetags/dashboard_tags.py
@@ -1,7 +1,21 @@
+from datetime import datetime
+
 from django import template
+
 register = template.Library()
 
 
 @register.filter
 def to_megabit(value):
-    return float(value) / 120000.00
+    return float(value) / 125000.00
+
+
+@register.filter
+def nice_dt(dt_string):
+    fmt = '%Y-%m-%dT%H:%M:%SZ'
+    try:
+        dt = datetime.strptime(dt_string, fmt)
+    except Exception as e:
+        dt = None
+        return ''
+    return dt.strftime('%H:%M on %b %-d.')
