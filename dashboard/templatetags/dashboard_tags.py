2dc6562f3b726800dfcecb73cba44577ec2b7c3d
Aleksandar Ristic <aleksandar1983@gmail.com>
2022-12-06 11:18:13 +0100

    Tweaks

--- dashboard/templatetags/dashboard_tags.py
+++ dashboard/templatetags/dashboard_tags.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timedelta
 
 from django import template
@@ -15,7 +16,7 @@ def to_dt(dt_string, hour_delta=1):
     fmt = '%Y-%m-%dT%H:%M:%SZ'
     try:
         dt = datetime.strptime(dt_string, fmt) + timedelta(hours=hour_delta)  # hack; need to parse TZ
-    except Exception as e:
+    except ValueError as e:
+        logging.error(f'Error parsing datetime: {e}')
         dt = None
-        return ''
     return dt
